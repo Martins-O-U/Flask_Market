2441aac391ec2fcdd26075d93fbfb330bdaa9476
HP <martinsonyedikachi@gmail.com>
2021-03-26 10:47:13 +0100

    Market file initiated

--- MarketPlace.py
+++ MarketPlace.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('Home.html')
+
+@app.route('/about')
+def about_page():
+    return "<h2>This is the about page</h2>"
+
+@app.route('/market')
+def market():
+    items = [
+        {"id":1, "name":"Phone", "barcode": 12345, "price": 500},
+        {"id":2, "name":"Laptop", "barcode": 123456, "price": 1500},
+        {"id":3, "name":"Keyboard", "barcode": 1234567, "price": 100}
+    ]
+    return render_template('MarketStore.html', items_name=items)
     
